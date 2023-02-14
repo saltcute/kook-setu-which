6f418becf3291d98c23f5aec0e30df200f34875e
Hexona <xuanjiap@lolicon.ac.cn>
2023-02-14 02:16:28 -0500

    fix: command.name

--- src/commands/which/which.search.app.ts
+++ src/commands/which/which.search.app.ts
@@ -4,10 +4,7 @@ import auth from 'configs/auth';
 import { bot } from 'init/client';
 
 class WhichSearch extends BaseCommand {
-    code = 'search'; // 只是用作标记
-    trigger = 'search'; // 用于触发的文字
-    help = '`.echo kmd 内容`'; // 帮助文字
-    intro = '复读你所说的文字, 并用kmarkdown格式返回。';
+    name = 'search';
     func: CommandFunction<BaseSession, any> = async (session) => {
         axios({
             url: "https://saucenao.com/search.php",
